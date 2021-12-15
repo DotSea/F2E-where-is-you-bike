3a6b22fcb8489fba4d5a9b6e656ad5581d4ef821
DotSea <85677992+DavidFu95@users.noreply.github.com>
2021-12-15 16:28:12 +0800

    refactor: redesign function of getting data  and displaying marker of map. In order to make function more precise and clear, I rewrite the function in BikeMap.vue, including getting user position, setting marker of user and station, going backtion to current station.

--- src/components/BikeMap.vue
+++ src/components/BikeMap.vue
@@ -43,6 +43,23 @@ import L from 'leaflet';
 import CurrentLocation from '../assets/svg/current-location.svg';
 import SearchStation from '../assets/svg/search-station-mobile.svg';
 
+const authorizationHeader = () => {
+  const AppID = process.env.VUE_APP_API_ID;
+  const AppKey = process.env.VUE_APP_API_KEY;
+  const GMTString = new Date().toGMTString();
+  const ShaObj = new jsSHA('SHA-1', 'TEXT');
+  ShaObj.setHMACKey(AppKey, 'TEXT');
+  ShaObj.update(`x-date: ${GMTString}`);
+  const HMAC = ShaObj.getHMAC('B64');
+  const Authorization = `hmac username="${AppID}", algorithm="hmac-sha1", headers="x-date", signature="${HMAC}"`;
+  return { Authorization, 'X-Date': GMTString };
+};
+
+const axiosInstance = axios.create({
+  baseURL: 'https://ptx.transportdata.tw/MOTC/v2/Bike',
+  headers: authorizationHeader(),
+});
+
 export default {
   name: 'BikeMap',
   components: { CurrentLocation, SearchStation },
@@ -84,6 +101,7 @@ export default {
     async chooseStation(station) {
       await this.map.setView([station.coordinate.PositionLat, station.coordinate.PositionLon], 17);
       await this.getNearbyStation(station.coordinate.PositionLat, station.coordinate.PositionLon);
+      this.setStationMarker(this.nearbyStationCoord);
       this.markers.eachLayer((marker) => {
         if (marker.options.id === station.stationUID) {
           marker.openPopup();
@@ -122,6 +140,7 @@ export default {
     },
     // init map
     setMap(latitude, longitude) {
+      // 設置leaflet以及圖資
       this.map = L.map('map', { zoomAnimation: false, tap: false }).setView(
         [latitude, longitude],
         17,
@@ -139,11 +158,14 @@ export default {
           accessToken: process.env.VUE_APP_MAPTOKEN,
         },
       ).addTo(this.map);
-
+      this.calculateDragDistanceOfMap();
+    },
+    // 記錄地圖拖曳的起迄點，如果起訖點的經度或緯度相差超過0.003，則重新顯示迄點的附近站牌
+    calculateDragDistanceOfMap() {
       this.map.on('dragstart', () => {
         this.dragDistance.startCoord = [this.map.getCenter().lat, this.map.getCenter().lng];
       });
-      this.map.on('dragend', () => {
+      this.map.on('dragend', async () => {
         this.dragDistance.endCoord = [this.map.getCenter().lat, this.map.getCenter().lng];
         const dragDistance = [
           this.dragDistance.startCoord[0] - this.dragDistance.endCoord[0],
@@ -151,17 +173,19 @@ export default {
         ];
         if (dragDistance.some((item) => Math.abs(item) > 0.003)) {
           this.markers.clearLayers();
-          this.getNearbyStation(this.dragDistance.endCoord[0], this.dragDistance.endCoord[1]);
+          await this.getNearbyStation(this.dragDistance.endCoord[0], this.dragDistance.endCoord[1]);
+          this.setStationMarker(this.nearbyStationCoord);
         }
       });
     },
-    // set user location
+    // 設置user位置的marker
     setUserLocationMarker(latitude, longitude) {
       const iconSVG = '<svg width="12" height="16" viewBox="0 0 12 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6 0.142857C2.95929 0.142857 0.5 2.60214 0.5 5.64286C0.5 9.76786 6 15.8571 6 15.8571C6 15.8571 11.5 9.76786 11.5 5.64286C11.5 2.60214 9.04071 0.142857 6 0.142857ZM6 7.60714C4.91571 7.60714 4.03571 6.72714 4.03571 5.64286C4.03571 4.55857 4.91571 3.67857 6 3.67857C7.08429 3.67857 7.96429 4.55857 7.96429 5.64286C7.96429 6.72714 7.08429 7.60714 6 7.60714Z" fill="black"/></svg>';
       const myicon = L.divIcon({
         html: iconSVG,
         className: '',
       });
+      /// 兩種marker
       this.userMarker = new L.marker([latitude, longitude], { icon: myicon });
       this.circleMarker = new L.circleMarker([latitude, longitude], {
         radius: 30,
@@ -171,55 +195,60 @@ export default {
         fill: true,
         fillOpacity: 0.1,
       });
+      /// 將user marker加入地圖
       this.map.addLayer(this.userMarker);
       this.map.addLayer(this.circleMarker);
     },
-    backToCurrentLocation() {
+    // 回到使用者目前位置
+    async backToCurrentLocation() {
       this.map.setView([this.coordinate.latitude, this.coordinate.longitude], 17);
+      /// 清除拖曳畫面上的站點marker
       this.markers.clearLayers();
-      this.getNearbyStation(this.coordinate.latitude, this.coordinate.longitude);
-      this.dragDistance.startCoord = [this.map.getCenter().lat, this.map.getCenter().lng];
+      /// 重新取得使用者位置附近站點資訊
+      await this.getNearbyStation(this.coordinate.latitude, this.coordinate.longitude);
+      this.setStationMarker(this.nearbyStationCoord);
+      this.dragDistance.startCoord = [this.coordinate.latitude, this.coordinate.longitude];
     },
-    // set bike Station info
+    // 取得附近站點資訊
     async getNearbyStation(latitude, longitude) {
       try {
-        // get nearby station info
-        const stationInfo = await axios.get(
-          `https://ptx.transportdata.tw/MOTC/v2/Bike/Station/NearBy?$spatialFilter=nearby(${latitude}%2C%20${longitude}%2C1000)&$format=JSON`,
-          { headers: this.authorizationHeader },
+        /// 取得附近方圓一公里內站點名稱等基本資料
+        const stationInfo = await axiosInstance.get(
+          `/Station/NearBy?$spatialFilter=nearby(${latitude}%2C%20${longitude}%2C1000)&$format=JSON`,
         );
-        const stationData = stationInfo.data;
-        // get nearby station current status
-        const stationStatus = await axios.get(
-          `https://ptx.transportdata.tw/MOTC/v2/Bike/Availability/NearBy?$spatialFilter=nearby(${latitude}%2C%20${longitude}%2C1000)&$format=JSON`,
-          { headers: this.authorizationHeader },
+        const infoData = stationInfo.data;
+        ///  取得附近方圓一公里內站點可借車輛/剩餘車位數等狀態
+        const stationStatus = await axiosInstance.get(
+          `/Availability/NearBy?$spatialFilter=nearby(${latitude}%2C%20${longitude}%2C1000)&$format=JSON`,
         );
-        const availableData = stationStatus.data;
-        availableData.forEach((availableItem) => {
-          stationData.forEach((stationItem) => {
-            if (availableItem.StationUID === stationItem.StationUID) {
-              availableItem.StationName = stationItem.StationName;
-              availableItem.StationAddress = stationItem.StationAddress;
-              availableItem.StationPosition = stationItem.StationPosition;
+        /// 兩種資料比對：若UID相同，則合併，以便後續資料處理
+        const statusData = stationStatus.data;
+        statusData.forEach((statusItem) => {
+          infoData.forEach((infoItem) => {
+            if (statusItem.StationUID === infoItem.StationUID) {
+              statusItem.StationName = infoItem.StationName;
+              statusItem.StationAddress = infoItem.StationAddress;
+              statusItem.StationPosition = infoItem.StationPosition;
             }
           });
         });
         // Some station doesn't have coordinate, need to exclude
-        this.nearbyStationCoord = availableData.filter((item) => item.StationPosition);
-        this.setStationMarker(this.nearbyStationCoord);
+        this.nearbyStationCoord = statusData.filter((item) => item.StationPosition);
       } catch (e) {
         console.log(e);
       }
     },
-
+    // 設置站牌的marker
     setStationMarker(nearbyStation) {
       nearbyStation.forEach((item) => {
         const coord = [item.StationPosition.PositionLat, item.StationPosition.PositionLon];
+        /// marker 的icon
         const stationIcon = new L.divIcon({
           html: this.stationSVG,
-          className: `${this.isSelectRent ? 'station-icon' : 'station-icon-parking'}`,
+          className: `${this.isSelectRent ? '' : 'station-icon-parking'}`,
           iconAnchor: [18, 21],
         });
+        /// icon上的數字
         const iconNum = new L.divIcon({
           html: `<p>${this.isSelectRent ? item.AvailableRentBikes : item.AvailableReturnBikes}</p>`,
           className: `${this.isSelectRent ? 'icon-num' : 'icon-num-parking'}`,
@@ -231,7 +260,6 @@ export default {
           .setContent(
             `<p>${item.StationName.Zh_tw}</p><p>可借車輛<span class="popup-number">${item.AvailableRentBikes}</span></p><p>可停空位<span class="popup-number">${item.AvailableReturnBikes}</span></p>`,
           );
-
         const marker = (icon) => new L.marker(coord, { id: item.StationUID }).setIcon(icon);
         this.markers.addLayer(marker(stationIcon).bindPopup(pop).openPopup());
         this.markers.addLayer(marker(iconNum).bindPopup(pop).openPopup());
@@ -295,13 +323,20 @@ export default {
   created() {
     try {
       if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position) => {
+        navigator.geolocation.getCurrentPosition(async (position) => {
+          //  取得使用者目前位置，並儲存使用者位置座標
           this.coordinate.latitude = position.coords.latitude;
           this.coordinate.longitude = position.coords.longitude;
+          // 設置leaflet以及圖資
           this.setMap(this.coordinate.latitude, this.coordinate.longitude);
+          // 設置user位置的icon
           this.setUserLocationMarker(this.coordinate.latitude, this.coordinate.longitude);
-          this.getNearbyStation(this.coordinate.latitude, this.coordinate.longitude);
-          this.dragDistance.startCoord = [this.map.getCenter().lat, this.map.getCenter().lng];
+          // 向TDX取得附近站點的資料
+          await this.getNearbyStation(this.coordinate.latitude, this.coordinate.longitude);
+          // 設置站點的marker
+          this.setStationMarker(this.nearbyStationCoord);
+          // 將 user位置設為拖曳點的起始座標
+          this.dragDistance.startCoord = [this.coordinate.latitude, this.coordinate.longitude];
         });
       }
     } catch (e) {
