d5b31f80c5256368943deab0035d1d8c2c774817
DotSea <85677992+DavidFu95@users.noreply.github.com>
2022-01-04 22:02:25 +0800

    style: rename name of button in home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,9 +8,7 @@
     <div class="button-group">
       <router-link to="/bike"> <button class="home-button">尋找Youbike</button></router-link>
       <router-link to="/bikeway"> <button class="home-button">查詢自行車道</button></router-link>
-      <router-link to="/tourism">
-        <button class="home-button">附近景點、美食(施工中)</button></router-link
-      >
+      <router-link to="/tourism"> <button class="home-button">附近景點、美食</button></router-link>
     </div>
     <div class="copyright">Where’s YouBike © Code: DotSea / Design: KT</div>
   </div>
